892fc1e7661c397e50f92a3c71374a1a594f9e12
Sergey Ponomarev <me@sergey-ponomarev.ru>
2019-02-05 15:12:23 -0500

    Allow overriding default config file path via ENV with Rails integration

--- lib/anyway/config.rb
+++ lib/anyway/config.rb
@@ -156,14 +156,12 @@ module Anyway # :nodoc:
     end
 
     def load_from_file(config)
-      config_path = Anyway.env.fetch(env_prefix).delete('conf') ||
-                    "./config/#{config_name}.yml"
       config.deep_merge!(parse_yml(config_path) || {}) if config_path && File.file?(config_path)
       config
     end
 
     def load_from_env(config)
-      config.deep_merge!(Anyway.env.fetch(env_prefix))
+      config.deep_merge!(env_part)
       config
     end
 
@@ -187,6 +185,18 @@ module Anyway # :nodoc:
     end
 
     private
+
+    def env_part
+      Anyway.env.fetch(env_prefix)
+    end
+
+    def config_path
+      env_part.delete('conf') || default_config_path
+    end
+
+    def default_config_path
+      "./config/#{config_name}.yml"
+    end
 
     def set_value(key, val)
       send("#{key}=", val) if respond_to?(key)
