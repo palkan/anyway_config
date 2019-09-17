dd55c5065a0b2f0f79595c4bf6e58a6a10a418e9
Vladimir Dementyev <dementiev.vm@gmail.com>
2019-09-17 16:32:27 +0300

    refactor: make default_config_path configurable

--- lib/anyway_config.rb
+++ lib/anyway_config.rb
@@ -4,8 +4,6 @@ module Anyway # :nodoc:
   require "anyway/version"
 
   require "anyway/config"
-  require "anyway/rails/config" if defined?(::Rails::VERSION)
-  require "anyway/env"
 
   # Use Settings name to not confuse with Config.
   #
@@ -15,10 +13,16 @@ module Anyway # :nodoc:
       # Define whether to load data from
       # *.yml.local (or credentials/local.yml.enc)
       attr_accessor :use_local_files
+
+      # Return a path to YML config file given the config name
+      attr_accessor :default_config_path
     end
 
     # By default, use local files only in development (that's the purpose if the local files)
     self.use_local_files = (ENV["RACK_ENV"] == "development" || ENV["RAILS_ENV"] == "development")
+
+    # By default, consider configs are stored in the ./config folder
+    self.default_config_path = ->(name) { "./config/#{name}.yml" }
   end
 
   class << self
@@ -27,6 +31,8 @@ module Anyway # :nodoc:
     end
   end
 
+  require "anyway/rails/config" if defined?(::Rails::VERSION)
+  require "anyway/env"
   require "anyway/railtie" if defined?(::Rails::VERSION)
   require "anyway/testing" if ENV["RACK_ENV"] == "test" || ENV["RAILS_ENV"] == "test"
 end
