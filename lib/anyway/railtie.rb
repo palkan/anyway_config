2c887659cf371c1f8e82fb98a1c792cfeb24c3e9
Vladimir Dementyev <dementiev.vm@gmail.com>
2019-10-18 23:18:45 +0300

    feat: freeze loaders after Rails app initialized

--- lib/anyway/railtie.rb
+++ lib/anyway/railtie.rb
@@ -7,5 +7,8 @@ module Anyway # :nodoc:
 
     # Allow autoloading of app/configs in configuration files
     ActiveSupport::Dependencies.autoload_paths << "app/configs"
+
+    # Make sure loaders are not changed in runtime
+    config.after_initialize { Anyway.loaders.freeze }
   end
 end
