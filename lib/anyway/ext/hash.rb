037b5514848483d0ecb0599bc849da7ae78e905e
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-03-13 21:24:43 -0400

    Fix Hash#deep_merge

--- lib/anyway/ext/hash.rb
+++ lib/anyway/ext/hash.rb
@@ -7,18 +7,14 @@ module Anyway
       refine ::Hash do
         # From ActiveSupport http://api.rubyonrails.org/classes/Hash.html#method-i-deep_merge
         def deep_merge!(other_hash)
-          other_hash.each_pair do |current_key, other_value|
-            this_value = self[current_key]
-
+          merge!(other_hash) do |key, this_value, other_value|
             if this_value.is_a?(::Hash) && other_value.is_a?(::Hash)
               this_value.deep_merge!(other_value)
               this_value
             else
-              self[current_key] = other_value
+              other_value
             end
           end
-
-          self
         end
 
         def stringify_keys!
