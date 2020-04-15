60a74042c1cd27979fd277c4ca422059fd4df3e2
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-04-15 22:06:15 +0300

    fix(rails): load railtie after configuring loaders

--- lib/anyway/rails.rb
+++ lib/anyway/rails.rb
@@ -8,9 +8,12 @@ end
 require "anyway/rails/settings"
 require "anyway/rails/config"
 require "anyway/rails/loaders"
-require "anyway/railtie"
 
 # Configure Rails loaders
 Anyway.loaders.override :yml, Anyway::Rails::Loaders::YAML
 Anyway.loaders.insert_after :yml, :secrets, Anyway::Rails::Loaders::Secrets
 Anyway.loaders.insert_after :secrets, :credentials, Anyway::Rails::Loaders::Credentials
+
+# Load Railties after configuring loaders.
+# The application could be already initialized, and that would make `Anyway.loaders` frozen
+require "anyway/railtie"
