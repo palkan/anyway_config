f066446e195594867053c8460461075e6ce553e3
Dmitriy Nemykin <dmitriy.nemykin@rt.ru>
2021-12-22 17:49:57 +0300

    Added support for env configs in base yaml loader

--- lib/anyway/rails/loaders/yaml.rb
+++ lib/anyway/rails/loaders/yaml.rb
@@ -4,27 +4,14 @@ module Anyway
   module Rails
     module Loaders
       class YAML < Anyway::Loaders::YAML
-        def load_base_yml(*)
-          parsed_yml = super
-          return parsed_yml unless environmental?(parsed_yml)
-
-          env_config = parsed_yml[::Rails.env] || {}
-          return env_config if Anyway::Settings.default_environmental_key.blank?
-
-          default_config = parsed_yml[Anyway::Settings.default_environmental_key] || {}
-          Utils.deep_merge!(default_config, env_config)
-        end
-
         private
 
         def environmental?(parsed_yml)
           return true unless Settings.future.unwrap_known_environments
-          # likely
-          return true if parsed_yml.key?(::Rails.env)
           # less likely
           return true if ::Rails.application.config.anyway_config.known_environments.any? { parsed_yml.key?(_1) }
-          # strange, but still possible
-          Anyway::Settings.default_environmental_key.present? && parsed_yml.key?(Anyway::Settings.default_environmental_key)
+
+          super parsed_yml
         end
 
         def relative_config_path(path)
--- spec/loaders/yaml_spec.rb
+++ spec/loaders/yaml_spec.rb
@@ -9,40 +9,13 @@ describe Anyway::Loaders::YAML do
 
   let(:options) { {config_path: path, local: false, some_other: "value"} }
 
-  it "parses YAML" do
-    expect(subject).to eq(
-      {
-        "host" => "test.host",
-        "user" => {
-          "name" => "root",
-          "password" => "root"
-        },
-        "port" => 9292
-      }
-    )
-  end
+  context 'in non-Rails apps' do
+    before { allow(Anyway::Settings).to receive(:current_environment).and_return(nil) }
 
-  specify "when no ERB available" do
-    hide_const("ERB")
-    expect(subject).to eq(
-      {
-        "host" => "test.host",
-        "user" => {
-          "name" => "root",
-          "password" => "root"
-        },
-        "port" => "<%= ENV['ANYWAY_COOL_PORT'] || 9292 %>"
-      }
-    )
-  end
-
-  context "when local is enabled" do
-    let(:options) { {config_path: path, local: true, some_other: "value"} }
-
-    specify do
+    it "parses YAML" do
       expect(subject).to eq(
         {
-          "host" => "local.host",
+          "host" => "test.host",
           "user" => {
             "name" => "root",
             "password" => "root"
@@ -51,21 +24,52 @@ describe Anyway::Loaders::YAML do
         }
       )
     end
-  end
 
-  context "when file doesn't exist" do
-    let(:options) { {config_path: File.join(__dir__, "no.yml")} }
-
-    it "returns empty hash" do
-      expect(subject).to eq({})
+    specify "when no ERB available" do
+      hide_const("ERB")
+      expect(subject).to eq(
+        {
+          "host" => "test.host",
+          "user" => {
+            "name" => "root",
+            "password" => "root"
+          },
+          "port" => "<%= ENV['ANYWAY_COOL_PORT'] || 9292 %>"
+        }
+      )
     end
-  end
 
-  context "when file is empty" do
-    let(:options) { {config_path: File.join(__dir__, "../config/empty.yml")} }
+    context "when local is enabled" do
+      let(:options) { {config_path: path, local: true, some_other: "value"} }
 
-    it "returns empty hash" do
-      expect(subject).to eq({})
+      specify do
+        expect(subject).to eq(
+          {
+            "host" => "local.host",
+            "user" => {
+              "name" => "root",
+              "password" => "root"
+            },
+            "port" => 9292
+          }
+        )
+      end
+    end
+
+    context "when file doesn't exist" do
+      let(:options) { {config_path: File.join(__dir__, "no.yml")} }
+
+      it "returns empty hash" do
+        expect(subject).to eq({})
+      end
+    end
+
+    context "when file is empty" do
+      let(:options) { {config_path: File.join(__dir__, "../config/empty.yml")} }
+
+      it "returns empty hash" do
+        expect(subject).to eq({})
+      end
     end
   end
 end
