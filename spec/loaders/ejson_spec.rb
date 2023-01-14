339adb6cd77267d3f976464d27d0298722f1e27a
Maxim Rydkin <maks.rydkin@gmail.com>
2023-01-14 11:58:49 +0300

    added support for environments

--- spec/loaders/ejson_spec.rb
+++ spec/loaders/ejson_spec.rb
@@ -7,13 +7,14 @@ describe Anyway::Loaders::EJSON do
 
   let(:options) { {name:, ejson_parser:, local: false} }
 
-  # let(:path) { File.join(__dir__, "../config/cool.yml") }
   let(:name) { "clever" }
 
   let(:ejson_parser) do
-    adapter = instance_double(Anyway::EJSONParser)
-    allow(adapter).to receive(:call).with(config_path).and_return(ejson_parsed_result)
-    adapter
+    parser = instance_double(Anyway::EJSONParser)
+    allow(parser).to receive(:call).with(config_path).and_return(ejson_parsed_result)
+    allow(parser).to receive(:call).with(local_config_path).and_return(local_ejson_parsed_result)
+    allow(parser).to receive(:call).with(development_config_path).and_return(development_ejson_parsed_result)
+    parser
   end
   let(:config_path) { "#{Anyway::Settings.app_root}/config/secrets.ejson" }
   let(:ejson_parsed_result) do
@@ -34,48 +35,57 @@ describe Anyway::Loaders::EJSON do
     }
   end
 
+  let(:local_config_path) { "#{Anyway::Settings.app_root}/config/secrets.local.ejson" }
+  let(:local_ejson_parsed_result) do
+    {
+      "_public_key" => "any_public_key",
+      "clever" =>
+        {
+          "_username" => "local_username",
+          "password" => "local_password"
+        },
+      "cool" =>
+        {
+          "_username" => "5678username5678",
+          "password" => "5678password5678"
+        }
+    }
+  end
+
+  let(:development_config_path) { "#{Anyway::Settings.app_root}/config/development/secrets.ejson" }
+  let(:development_ejson_parsed_result) do
+    {
+      "_public_key" => "any_public_key",
+      "clever" =>
+        {
+          "_username" => "development_username",
+          "password" => "development_password"
+        },
+      "cool" =>
+        {
+          "_username" => "8765username8765",
+          "password" => "8765password8765"
+        }
+    }
+  end
+
   context "for apps without environments" do
     before { allow(Anyway::Settings).to receive(:current_environment).and_return(nil) }
 
     it "parses EJSON" do
       expect(subject).to eq(
-        {
-          "username" => "1234username",
-          "password" => "1234password"
-        }
+        "username" => "1234username",
+        "password" => "1234password"
       )
     end
 
     context "when local is enabled" do
       let(:options) { {name:, ejson_parser:, local: true} }
 
-      let(:local_config_path) { "#{Anyway::Settings.app_root}/config/secrets.local.ejson" }
-      let(:local_ejson_parsed_result) do
-        {
-          "_public_key" => "any_public_key",
-          "clever" =>
-            {
-              "_username" => "1234username1234",
-              "password" => "1234password1234"
-            },
-          "cool" =>
-            {
-              "_username" => "5678username5678",
-              "password" => "5678password5678"
-            }
-        }
-      end
-
-      before do
-        allow(ejson_parser).to receive(:call).with(local_config_path).and_return(local_ejson_parsed_result)
-      end
-
       it "parses local EJSON config" do
         expect(subject).to eq(
-          {
-            "username" => "1234username1234",
-            "password" => "1234password1234"
-          }
+          "username" => "local_username",
+          "password" => "local_password"
         )
       end
     end
@@ -105,35 +115,36 @@ describe Anyway::Loaders::EJSON do
     end
   end
 
-  # context "with environment" do
-  #   let(:path) { File.join(__dir__, "../config/cool.env.yml") }
+  context "with environment" do
+    before { allow(Anyway::Settings).to receive(:current_environment).and_return("development") }
 
-  #   before { allow(Anyway::Settings).to receive(:current_environment).and_return("development") }
+    specify do
+      expect(subject).to eq(
+        "username" => "development_username",
+        "password" => "development_password"
+      )
+    end
 
-  #   context "loads all keys under current environment section" do
-  #     specify do
-  #       expect(subject).to eq("host" => "localhost",
-  #         "user" => "user",
-  #         "log_level" => "debug",
-  #         "port" => 80,
-  #         "mailer" => {
-  #           "host" => "mailhog"
-  #         })
-  #     end
+    context 'when there is no development config' do
+      let(:development_ejson_parsed_result) { nil }
 
-  #     context "using local file config" do
-  #       before { options.merge!(local: true) }
+      it 'returns data from default file' do
+        expect(subject).to eq(
+          "username" => "1234username",
+          "password" => "1234password"
+        )
+      end
+    end
 
-  #       it "overrides env config" do
-  #         expect(subject).to eq("host" => "localhost",
-  #           "user" => "user",
-  #           "log_level" => "info",
-  #           "port" => 443,
-  #           "mailer" => {
-  #             "host" => "mail.google.com"
-  #           })
-  #       end
-  #     end
-  #   end
-  # end
+    context "using local file config" do
+      let(:options) { {name:, ejson_parser:, local: true} }
+
+      it "overrides config with local data" do
+        expect(subject).to eq(
+          "username" => "local_username",
+          "password" => "local_password"
+        )
+      end
+    end
+  end
 end
