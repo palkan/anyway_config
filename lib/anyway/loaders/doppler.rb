7b6ac818f4e5a32ba915fbf387102f00e857cb46
SupDex <symeton@gmail.com>
2023-01-18 19:04:52 +0300

    use _options in arguments

--- lib/anyway/loaders/doppler.rb
+++ lib/anyway/loaders/doppler.rb
@@ -12,7 +12,7 @@ module Anyway
       DOPPLER_REQUEST_ERROR = Class.new(StandardError)
       DOPPLER_JSON_FORMAT_URL = "https://api.doppler.com/v3/configs/config/secrets/download"
 
-      def call(env_prefix:, **)
+      def call(env_prefix:, **_options)
         env_payload = parse_doppler_response
 
         env = ::Anyway::Env.new(type_cast: ::Anyway::NoCast, env_container: env_payload)
