f2f022dd739c5430ea0f831d394b7021e786fc64
Maxim Rydkin <maks.rydkin@gmail.com>
2023-01-18 10:13:19 +0300

    moved `which` from parser to config to ask it only once on gem loading phase

---
+++ lib/anyway/utils/which.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Anyway
+  module Utils
+    # Cross-platform solution
+    # taken from https://stackoverflow.com/a/5471032
+    def self.which(cmd)
+      exts = ENV["PATHEXT"] ? ENV["PATHEXT"].split(";") : [""]
+      ENV["PATH"].split(File::PATH_SEPARATOR).each do |path|
+        exts.each do |ext|
+          exe = File.join(path, "#{cmd}#{ext}")
+          return exe if File.executable?(exe) && !File.directory?(exe)
+        end
+      end
+      nil
+    end
+  end
+end
