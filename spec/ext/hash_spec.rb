cb8ce2738a8928bd90ac8da84cd6b0e9708b744f
Maxim Rydkin <maks.rydkin@gmail.com>
2023-01-18 10:53:11 +0300

    added test

--- spec/ext/hash_spec.rb
+++ spec/ext/hash_spec.rb
@@ -36,4 +36,34 @@ describe Anyway::Ext::Hash do
       expect(source.stringify_keys!).to eq(expected_result)
     end
   end
+
+  describe "#deep_transform_keys" do
+    let(:source) do
+      {
+        a: 1,
+        'b' => "hello",
+        c:
+          {
+            id: 1
+          }
+      }
+    end
+
+    let(:expected_result) do
+      {
+        "a - Symbol" => 1,
+        "b - String" => "hello",
+        "c - Symbol" =>
+          {
+            "id - Symbol" => 1
+          }
+      }
+    end
+
+    it 'transforms all keys' do
+      result = source.deep_transform_keys { |key| "#{key} - #{key.class}" }
+
+      expect(result).to eq(expected_result)
+    end
+  end
 end
