19029e8331d2a7db3456bb2548f184a28dad295f
tagirahmad <tagirbpl@gmail.com>
2023-04-01 11:36:13 +0300

    added #as_env which converts a config instance to the ENV-like hash

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -347,6 +347,16 @@ describe Anyway::Config, type: :config do
         end
       end
     end
+
+    describe "#as_env" do
+      it "returns ENV-like hash" do
+        expect(conf.as_env).to eq({"COOL_HOST" => "test.host",
+                                   "COOL_META__KOT" => "leta",
+                                   "COOL_PORT" => "8080",
+                                   "COOL_USER__NAME" => "secret man",
+                                   "COOL_USER__PASSWORD" => "root"})
+      end
+    end
   end
 
   context "without Rails", :norails do
