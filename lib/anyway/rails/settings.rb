a57db40f6149f11726543f22b657554aacf357d5
Vladimir Dementyev <palkan@hey.com>
2023-05-02 03:28:31 -0400

    fix: check setting up autoloader in older Rails
    There is not Zeitwerk-compatible inflector in Rails 5, so we ignore.

--- lib/anyway/rails/settings.rb
+++ lib/anyway/rails/settings.rb
@@ -11,9 +11,10 @@ module Anyway
   class Settings
     class << self
       attr_reader :autoload_static_config_path, :autoloader
+      attr_writer :autoload_via_zeitwerk
 
-      if defined?(::Zeitwerk)
-        def autoload_static_config_path=(val)
+      def autoload_static_config_path=(val)
+        if autoload_via_zeitwerk
           raise "Cannot setup autoloader after application has been initialized" if ::Rails.application.initialized?
 
           return unless ::Rails.root.join(val).exist?
@@ -28,18 +29,16 @@ module Anyway
           # and Rails 7 https://github.com/rails/rails/blob/5462fbd5de1900c1b1ce1c9dc11c1a2d8cdcd809/railties/lib/rails/autoloaders.rb#L15
           @autoloader = Zeitwerk::Loader.new.tap do |loader|
             loader.tag = "anyway.config"
-            loader.inflector = defined?(ActiveSupport::Dependencies::ZeitwerkIntegration::Inflector) ? ActiveSupport::Dependencies::ZeitwerkIntegration::Inflector : ::Rails::Autoloaders::Inflector
+
+            if defined?(ActiveSupport::Dependencies::ZeitwerkIntegration::Inflector)
+              loader.inflector = ActiveSupport::Dependencies::ZeitwerkIntegration::Inflector
+            elsif defined?(::Rails::Autoloaders::Inflector)
+              loader.inflector = ::Rails::Autoloaders::Inflector
+            end
             loader.push_dir(::Rails.root.join(val))
             loader.setup
           end
-        end
-
-        def cleanup_autoload_paths
-          return unless autoload_static_config_path
-          ActiveSupport::Dependencies.autoload_paths.delete(::Rails.root.join(autoload_static_config_path).to_s)
-        end
-      else
-        def autoload_static_config_path=(val)
+        else
           if autoload_static_config_path
             old_path = ::Rails.root.join(autoload_static_config_path).to_s
             ActiveSupport::Dependencies.autoload_paths.delete(old_path)
@@ -51,10 +50,19 @@ module Anyway
           ActiveSupport::Dependencies.autoload_paths << new_path
           ::Rails.application.config.eager_load_paths << new_path
         end
+      end
 
-        def cleanup_autoload_paths
-          :no_op
-        end
+      def cleanup_autoload_paths
+        return unless autoload_via_zeitwerk
+
+        return unless autoload_static_config_path
+        ActiveSupport::Dependencies.autoload_paths.delete(::Rails.root.join(autoload_static_config_path).to_s)
+      end
+
+      def autoload_via_zeitwerk
+        return @autoload_via_zeitwerk if instance_variable_defined?(:@autoload_via_zeitwerk)
+
+        @autoload_via_zeitwerk = defined?(::Zeitwerk)
       end
 
       def current_environment
